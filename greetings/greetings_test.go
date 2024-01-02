871d917342c7084068688e2ad7197a22bbbe8a22
lopan <tjohander@gmail.com>
2024-01-01 21:05:42 -0600

    Added in passing tests

---
+++ greetings/greetings_test.go
@@ -0,0 +1,22 @@
+package greetings
+
+import (
+	"regexp"
+	"testing"
+)
+
+func TestHelloName(t *testing.T) {
+	name := "Gladys"
+	want := regexp.MustCompile(`\b` + name + `\b`)
+	message, err := Hello("Gladys")
+	if !want.MatchString(message) || err != nil {
+		t.Fatalf(`Hello("Gladys") = %q, %v want match for %#q, nil`, message, err, want)
+	}
+}
+
+func TestHelloEmpty(t *testing.T) {
+	msg, err := Hello("")
+	if msg != "" || err == nil {
+		t.Fatalf(`Hello("Gladys" = %q, %v, want "", error`, msg, err)
+	}
+}
