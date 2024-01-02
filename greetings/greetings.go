1eaad377095ceff2e83aac39a5157b324b771c46
lopan <tjohander@gmail.com>
2024-01-01 19:51:59 -0600

    Basic slice exercise complete

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,23 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello %v, Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	// A Slice of Random Message Formats
+	formats := []string{
+		"Hi %v, Welcome!",
+		"Hail %v, Well met.",
+		"Ssup, %v",
+	}
+	return formats[rand.Intn(len(formats))]
 }
